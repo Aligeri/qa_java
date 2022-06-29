6fa54f4abcacc5e2f1178f502837c08c749881cc
Kseniya Krashenitsa <k.krashenitsa@tinkoff.ru>
2022-06-29 17:42:22 +0300

    FINAL: correct Lion class, added Jacoco tests + report

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -4,7 +4,21 @@ import java.util.List;
 
 public class Lion {
 
+    public int getKittens() {
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
     boolean hasMane;
+
+    Feline feline = new Feline();
+
+    public List<String> getFood() throws Exception {
+        return feline.getFood("Хищник");
+    }
 
     public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
@@ -14,19 +28,5 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
-    }
-
-    Feline feline = new Feline();
-
-    public int getKittens() {
-        return feline.getKittens();
-    }
-
-    public boolean doesHaveMane() {
-        return hasMane;
-    }
-
-    public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
     }
 }
